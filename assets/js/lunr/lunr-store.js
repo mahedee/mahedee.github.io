80a6e03d1670babde61e33d6d29cb1d7700261ef
Mahedee Hasan <mahedee.hasan@gmail.com>
2019-08-31 10:55:30 +0000

    Lastest site built on successful travis build 5 auto-pushed to github

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Layout: Author Override",
+        "excerpt":"Sites that may have content authored from various individuals can be accommodated by using data files. To attribute an author to a post or page that is different from the site author specified in _config.yml: Step 1. Create _data/authors.yml and add authors using the following format. Anything variables found under...","categories": ["Sample category"],
+        "tags": ["simple","move it"],
+        "url": "/sample%20category/layout-author-override/",
+        "teaser":null}]

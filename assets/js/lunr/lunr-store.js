9cb8a910015316da0ef58f8e04d8ef771199e6aa
Mahedee Hasan <mahedee.hasan@gmail.com>
2021-01-19 10:48:38 +0000

    Lastest site built on successful travis build 30 auto-pushed to github

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -14,10 +14,10 @@ var store = [{
         "tags": ["notes","unpublished"],
         "url": "/important-note-unpublished.html/",
         "teaser":null},{
-        "title": "Layout: Author Override",
-        "excerpt":"Sites that may have content authored from various individuals can be accommodated by using data files.   To attribute an author to a post or page that is different from the site author specified in _config.yml:   Step 1. Create _data/authors.yml and add authors using the following format. Anything variables found under author in _config.yml can be used (e.g. name, bio, avatar, author links, etc.).   # /_data/authors.yml  Billy Rick:   name        : \"Billy Rick\"   bio         : \"What do you want, jewels? I am a very extravagant man.\"   avatar      : \"/assets/images/bio-photo-2.jpg\"   links:     - label: \"Email\"       icon: \"fas fa-fw fa-envelope-square\"       url: \"mailto:billyrick@rick.com\"     - label: \"Website\"       icon: \"fas fa-fw fa-link\"       url: \"https://thewhip.com\"     - label: \"Twitter\"       icon: \"fab fa-fw fa-twitter-square\"       url: \"https://twitter.com/extravagantman\"  Cornelius Fiddlebone:   name        : \"Cornelius Fiddlebone\"   bio         : \"I ordered what?\"   avatar      : \"/assets/images/bio-photo.jpg\"   links:     - label: \"Email\"       icon: \"fas fa-fw fa-envelope-square\"       url: \"mailto:cornelius@thewhip.com\"     - label: \"Twitter\"       icon: \"fab fa-fw fa-twitter-square\"       url: \"https://twitter.com/rhymeswithsackit\"   Step 2. Assign one of the authors in authors.yml to a post or page you wish to override the site.author with.   Example: To assign Billy Rick as an author for a post the following YAML Front Matter would be applied:   author: Billy Rick  ","categories": ["Sample category"],
-        "tags": ["simple","move it"],
-        "url": "/Test-post.html/",
+        "title": "Markdown Syntax",
+        "excerpt":"Commenting on markdown   &lt;!-- Single or multiline commenting--&gt;   ","categories": ["Github"],
+        "tags": ["markdown","md"],
+        "url": "/markdown-syntax.html/",
         "teaser":null},{
         "title": "Getting Started with GitHub",
         "excerpt":"Getting Started with GitHub          Create a GitHub account.            Setting up git on your computer              Download and install the latest version of Git.       Set your username in Git.                Create a repository       Clone your repository    For example:     git clone https://github.com/mahedee/eSchool.git       Pull and push to repository   Pull command uses to get updated files from github repository to your local machine  git pull --rebase upstream master or  git pull   Push command uses to sync your local file to github repository   Add files for the commit  git add .  Write a message for the commit  git commit -m \"complete feature-joining\"  git push origin feature-joining or  git push  ","categories": ["Source Control"],
@@ -40,7 +40,7 @@ var store = [{
         "url": "/how-to-contribute-an-opensource-project.html/",
         "teaser":null},{
         "title": "Speaking at Career Adda at MBSTU",
-        "excerpt":"Speaking at Career Adda, 2020  Organized by CSE Family of MBSTU  Venue: Online/Virtual  Date: 21 August, 2020      ","categories": ["Speech","Technical Speech"],
+        "excerpt":" Organized by CSE Family of MBSTU  Venue: Online/Virtual  Date: 21 August, 2020      ","categories": ["Speech","Technical Speech"],
         "tags": ["speech","tech"],
         "url": "/speaking-at-career-adda-at-cse-family-of-mbstu.html/",
         "teaser":null},{
